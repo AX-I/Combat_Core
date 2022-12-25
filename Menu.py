8194a394a5ff6af9e9cf8a92fc995746f1c63eef
AX-I <36831950+AX-I@users.noreply.github.com>
2022-12-24 23:14:30 -0500

    Portability fixes

--- Menu.py
+++ Menu.py
@@ -250,9 +250,9 @@ class CombatMenu(Frame, ImgUtils.NPCanvas):
         n = self.openImageCover('../Assets/Noise5wa.png')
         nm = np.array(n.rotate(-90))
         a = nm[:,:,3]
-        a = a * (320/256) - 68
+        a = a * (320/256) - 64
         nm[:,:,3] = np.maximum(a, 0)
-        diff = n.size[1] - self.H
+        diff = max(1, n.size[1] - self.H)
         nm = nm[diff//2:-diff//2]
 
         self.bgNoise = self.makeCL('BgNoise', nm)
