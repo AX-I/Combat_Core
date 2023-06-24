AX-I/Combat_Core

commit 799ce6ef7f1877bcd79449538b393626f8fac3a5

Tweak stage materials

Ops_GL.py
@@ -194,6 +194,7 @@ class CLDraw:
             except: pass
 
         self.setupPost()
+        self.setupSSAO()
 
     def setupPost(self):
         w, h = self.outW, self.outH
@@ -448,7 +449,7 @@ class CLDraw:
         d = self.DRAW[tn]
         try:
             d['pScale'].write(np.float32(pScale))
-            d['pTime'].write(np.float32(time.time() - stTime))
+            d['pTime'].write(self.currTime)
         except KeyError:
             draw = ctx.program(vertex_shader=trisetupWave,
                                fragment_shader=drawSSR)
@@ -1043,6 +1044,8 @@ class CLDraw:
             if shaders[i] != self.oldShaders[i]:
                 self.changeShader(i, shaders[i], **kwargs)
 
+        self.currTime = np.float32(time.time() - self.stTime)
+
         # Write to readable depth buffer
         self.fboZ.use()
         self.fboZ.clear(red=2048.0, depth=1.0)
@@ -1162,7 +1165,7 @@ class CLDraw:
                 if 'special' in shaders[i]:
                     self.DRAW[i]['VV'].write(self.rawVM[0])
                 try:
-                    self.DRAW[i]['iTime'].write(np.float32(time.time() - self.stTime))
+                    self.DRAW[i]['iTime'].write(self.currTime)
                 except KeyError:
                     pass
 
@@ -1213,6 +1216,10 @@ class CLDraw:
                 self.DRAW[i]['vmat'].write(self.rawVM)
             else:
                 continue
+            if 'cull' in shaders[i]:
+                ctx.enable(moderngl.CULL_FACE)
+            else:
+                ctx.disable(moderngl.CULL_FACE)
 
             self.DRAW[i]['tex1'] = 0
             self.TEX[i].use(location=0)

Stages/Atrium.py
@@ -21,25 +21,27 @@ def setupStage(self):
                        subDiv=1, shadow="CR")
 
     for f in self.vtNames:
+        mat = self.matShaders[self.vtNames[f]]
         if "CV" in f:
-            self.matShaders[self.vtNames[f]]['emissive'] = 1.0
+            mat['emissive'] = 1.0
         if "BackgroundLight" in f:
-            self.matShaders[self.vtNames[f]]['add'] = 0.5
-            self.matShaders[self.vtNames[f]]['noline'] = True
-            self.matShaders[self.vtNames[f]]['cull'] = 1
+            mat['add'] = 0.5
+            mat['noline'] = True
+            mat['cull'] = 1
         if "FieldBackground" in f:
-            self.matShaders[self.vtNames[f]]['emissive'] = 2.0
+            mat['emissive'] = 2.0
         if 'AtlasG_opt' in f:
-            self.matShaders[self.vtNames[f]]['spec'] = 1
-            self.matShaders[self.vtNames[f]]['normal'] = 'AtlasG'
+            mat['spec'] = 1
+            mat['normal'] = 'AtlasG'
         if 'Copy' in f:
             self.ssrMTL = self.vtNames[f]
             self.matShaders[self.ssrMTL]['SSR'] = 2
             self.matShaders[self.ssrMTL]['roughness'] = 0.1
             self.matShaders[self.ssrMTL]['normal'] = 'AtlasG'
+            self.matShaders[self.ssrMTL]['cull'] = 1
         if 'AtlasW' in f:
-            self.matShaders[self.vtNames[f]]['spec'] = 1
-            self.matShaders[self.vtNames[f]]['roughness'] = 0.4
+            mat['spec'] = 1
+            mat['roughness'] = 0.4
 
     self.terrain = VertTerrain0([0,-0.6,0],
                                 PATH+"../Atrium/AtriumNav.png",

Stages/Strachan.py
@@ -260,11 +260,12 @@ def showPlatforms(self):
     for p in self.stagePlatforms:
         self.w.addCollider(Phys.CircleCollider(0.6, p))
         ps = CentripetalParticleSystem(p, (0, 0),
-                                       nParticles=60, lifespan=1200,
+                                       nParticles=30, lifespan=1200,
                                        randPos=0.08, vel=0.0, randVel=0.0,
-                                       size=0.1, opacity=0.05,
-                                       color=(0.2,0.2,0.2), randColor=0,
+                                       size=0.08, opacity=0.9,
+                                       color=(0.1,0.1,0.1), randColor=0,
                                        f=0.001, r=0.6, cc=1)
+        ps.shader = 2
         self.addParticleSystem(ps)
         self.stagePlatformPS.append(ps)
 

commit 975cb3caab54a78c0e0013e3154f5472ce57260a

Fix SSRopaque regression from 799ce6e

Ops_GL.py
@@ -1141,6 +1141,7 @@ class CLDraw:
                     pass
 
             elif 'SSR' in shader:
+                ctx.disable(moderngl.CULL_FACE)
                 ctx.disable(moderngl.DEPTH_TEST)
                 ctx.disable(moderngl.BLEND)
                 self.POSTFBO.clear(0.0, 0.0, 0.0, 0.0)