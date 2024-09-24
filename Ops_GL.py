AX-I/Combat_Core

commit 5202a7d9e0ebef1f4b520dc4735e221f0f46e50d

Physics optimizations (#22)
* Cache translate data

* Speed up attractors

* Properly disable colliders

20 projectiles: avg 7.6ms -> 1.9ms, peak 10ms -> 5ms

Ops_GL.py
@@ -182,6 +182,8 @@ class CLDraw:
 
         self.shaderParams = {'{REFL_LENGTH}':str(self.H)}
 
+        self.batchCache = {}
+
     def reloadShaders(self, **kwargs):
         while True:
             try:
@@ -392,11 +394,16 @@ class CLDraw:
             size = 8*4
             mEnd = max(a[2] for a in tb)
             mStart = min(a[1] for a in tb)
-            raw = self.VBO[tn].read(size=(mEnd-mStart)*size, offset=mStart*size)
-            dat = np.array(np.frombuffer(raw, 'float32')).reshape((mEnd-mStart, 8))
+            try:
+                dat = self.batchCache[tn]
+            except KeyError:
+                raw = self.VBO[tn].read()
+                dat = np.array(np.frombuffer(raw, 'float32')).reshape((-1, 8))
+                self.batchCache[tn] = dat
+
             for i in range(len(tb)):
-                dat[tb[i][1]-mStart:tb[i][2]-mStart,:3] += np.expand_dims(tb[i][0], 0)
-            self.VBO[tn].write(dat, offset=mStart*size)
+                dat[tb[i][1]:tb[i][2],:3] += np.expand_dims(tb[i][0], 0)
+            self.VBO[tn].write(dat[mStart:mEnd], offset=mStart*size)
 
     def translate(self, diff, cStart, cEnd, tn):
         size = 8*4

Multi.py
@@ -967,6 +967,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
         self.NPLAYERS += 1
         self.players.append(a)
         self.w.addRB(pv)
+        pv.disable()
 
         a["cheight"] = self.rpi[a["num"]][2] + 0.06 * (self.stage == 2)
 
@@ -1163,7 +1164,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
                                             usegravity=0, elasticity=0.8))
             self.srbs[-1].addCollider(Phys.BulletCollider(0.25, False,
                                                           rb=self.srbs[-1]))
-            self.srbs[-1].disabled = True
+            self.srbs[-1].disable()
             self.w.addRB(self.srbs[-1])
 
         self.sphereTrails = []
@@ -1186,7 +1187,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
             self.srbs[-1].addCollider(Phys.BulletCollider(0.5, False,
                                                           rb=self.srbs[-1],
                                                           damage=4))
-            self.srbs[-1].disabled = True
+            self.srbs[-1].disable()
             self.w.addRB(self.srbs[-1])
 
 
@@ -1252,7 +1253,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
                                                           rb=self.srbs[-1],
                                                           damage=6,
                                                           explode=True))
-            self.srbs[-1].disabled = True
+            self.srbs[-1].disable()
             self.w.addRB(self.srbs[-1])
 
         self.pickups = []
@@ -1278,7 +1279,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
                                                           rb=self.srbs[-1],
                                                           damage=3, hl=0,
                                                           blackHole=True))
-            self.srbs[-1].disabled = True
+            self.srbs[-1].disable()
             self.srbs[-1].colliders[0].onHit = lambda x: self.explode(x)
 
             self.w.addRB(self.srbs[-1])
@@ -1444,7 +1445,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
             a["Energy"] += self.COSTS[color]
             return False
 
-        self.srbs[cb].disabled = False
+        self.srbs[cb].enable()
         s = self.bulletSpeed
         if color == "black": s *= 0.66
         d = np.array([cos(a["cr"]), vh, sin(a["cr"])])
@@ -1987,6 +1988,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
                     for xn in a["ctexn"]:
                         self.matShaders[xn] = {'temp': self.matShaders[xn]}
                 a["pv"].pos[:] = -10.
+                a['pv'].disable()
                 a["Energy"] = 0
                 tr = 0.6 + 0.32 * min(40, (time.time() - a["deathTime"])*10) / 40
                 for xn in a["ctexn"]:
@@ -1998,7 +2000,9 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
                     g.step()
             elif tn not in actPlayers:
                 a["pv"].pos[:] = -10.
+                a['pv'].disable()
             else:
+                a['pv'].enable()
                 a["pv"].pos = a["b1"].offset[:3] + np.array([0,-0.5,0]) \
                               - a['animOffset'] + np.array([0,a['legIKoffset'],0])
                 a["Energy"] += 0.05 * self.frameTime
@@ -2085,7 +2089,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
                       (self.srbs[i].pos < self.BORDER[0])).any():
                     self.srbs[i].pos[:] = 0.
                     self.srbs[i].v[:] = 0.
-                    self.srbs[i].disabled = True
+                    self.srbs[i].disable()
 
 
         batch = {}
@@ -2096,7 +2100,7 @@ class CombatApp(ThreeDBackend, AI.AIManager, Anim.AnimManager):
                 print("NaN, disable")
                 self.srbs[i].pos[:] = 0.
                 self.srbs[i].v[:] = 0.
-                self.srbs[i].disabled = True
+                self.srbs[i].disable()
             diff = self.srbs[i].pos - lpi[self.lpCount[i]]
             if sum(diff*diff) > 0:
                 if s.texNum not in batch:

Phys.py
@@ -32,6 +32,8 @@ def eucDist(a, b):
     return math.sqrt(c)
 def eucLen(a):
     return math.sqrt(a[0]*a[0] + a[1]*a[1] + a[2]*a[2])
+def eucLen2(a):
+    return a[0]*a[0] + a[1]*a[1] + a[2]*a[2]
 
 def planeDist(a, n, p):
     return abs((a - p) @ n)
@@ -98,11 +100,13 @@ class CircleCollider(Collider):
 
     def collisionImpulse(self, obj):
         if obj.t == "Circle":
-            if eucDist(obj.pos, self.pos) == 0:
+            collDist = eucDist(obj.pos, self.pos)
+            if collDist == 0:
                 self.pos[1] += 0.01
+                collDist += 0.01
 
-            cdir = (obj.pos - self.pos) / eucDist(obj.pos, self.pos)
-            offset = self.r + obj.r - eucDist(obj.pos, self.pos)
+            cdir = (obj.pos - self.pos) / collDist
+            offset = self.r + obj.r - collDist
 
             if self.rb is None:
                 obj.rb.v = obj.rb.v - 2 * (obj.rb.v @ cdir) * cdir
@@ -159,7 +163,7 @@ class BulletCollider(CircleCollider):
                 self.disabled = True
                 self.rb.disabled = True
                 self.rb.pos[:] = 0.
-                self.rb.forces = []
+                self.rb.n_forces = 0
                 self.hc = 0
                 return False
             else:
@@ -175,7 +179,7 @@ class BulletCollider(CircleCollider):
             self.disabled = True
             self.rb.disabled = True
             self.rb.pos[:] = 0.
-            self.rb.forces = []
+            self.rb.n_forces = 0
             return False
 
         try:
@@ -184,7 +188,7 @@ class BulletCollider(CircleCollider):
                 self.disabled = True
                 self.rb.disabled = True
                 self.rb.pos[:] = 0.
-                self.rb.forces = []
+                self.rb.n_forces = 0
                 return False
         except AttributeError: pass
         return True
@@ -343,10 +347,10 @@ class RigidBody:
         else:               self.rotv = np.array(rotvel, dtype="float")
         
         self.el = elasticity
-            
-        self.forces = [[0, (-G * int(usegravity)), 0]]
-        if forces != None:
-            self.forces.extend(forces)
+
+        MAX_N_FORCES = 8
+        self.forces = np.zeros((MAX_N_FORCES, 3), 'float')
+        self.n_forces = 0
 
     def addCollider(self, collObj):
         collObj.rb = self
@@ -354,15 +358,25 @@ class RigidBody:
 
     def update(self, dt, attractors=[]):
         if self.disabled: return
-        self.forces = []
+        self.n_forces = 0
         if not self.noforces:
             for a in attractors:
                 i = attractors[a]
                 diff = (i["pos"] - self.pos)
-                self.forces.append(diff * i["m"] * self.M / np.sum(diff*diff))
-        self.v += np.sum(np.array(self.forces), axis=0) / self.M * dt
+                self.forces[self.n_forces] = diff * (i["m"] / eucLen2(diff))
+                self.n_forces += 1
+            self.v += np.sum(self.forces[:self.n_forces], axis=0) * dt
         
         self.pos += self.v * dt
+
+    def disable(self):
+        self.disabled = True
+        for c in self.colliders:
+            c.disabled = True
+    def enable(self):
+        self.disabled = False
+        for c in self.colliders:
+            c.disabled = False
 
 def collision(ma, mb, va, vb, ea, eb):
     """1-dimensional -> (vaf, vbf)"""

commit c6b4cc547d393a4f0410542313b83eb1216735f5

Cache rotate data
Fixes skis broken by 5202a7d

Ops_GL.py
@@ -419,12 +419,16 @@ class CLDraw:
             size = 8*4
             mEnd = max(a[2] for a in tb)
             mStart = min(a[1] for a in tb)
-            raw = self.VBO[tn].read(size=(mEnd-mStart)*size, offset=mStart*size)
-            dat = np.array(np.frombuffer(raw, 'float32')).reshape((mEnd-mStart, 8))
+            try:
+                dat = self.batchCache[tn]
+            except KeyError:
+                raw = self.VBO[tn].read()
+                dat = np.array(np.frombuffer(raw, 'float32')).reshape((-1, 8))
+                self.batchCache[tn] = dat
+
             for i in range(len(tb)):
-                dat[tb[i][1]-mStart:tb[i][2]-mStart,:3] = \
-                    dat[tb[i][1]-mStart:tb[i][2]-mStart,:3] @ tb[i][0]
-            self.VBO[tn].write(dat, offset=mStart*size)
+                dat[tb[i][1]:tb[i][2],:3] = dat[tb[i][1]:tb[i][2],:3] @ tb[i][0]
+            self.VBO[tn].write(dat[mStart:mEnd], offset=mStart*size)
 
     def rotate(self, rotMat, cStart, cEnd, tn):
         size = 8*4