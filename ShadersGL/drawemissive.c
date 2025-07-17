fd9a12637364f1867f88f89c2481c7d74cecc15c
AX-I <36831950+AX-I@users.noreply.github.com>
2025-07-17 14:20:02 -0400

    Cleanup shaders

--- ShadersGL/drawemissive.c
+++ ShadersGL/drawemissive.c
@@ -15,13 +15,12 @@ uniform sampler2D tex1;
 
 void main() {
 	float tz = 1 / depth;
-	vec3 norm = v_norm / tz;
 
     vec3 light = vec3(emPow);
     if (fadeDist != 0) {
       light *= vec3(clamp((tz-fadeBias)/(0.001+fadeDist), 0.f, 1.f));
     }
-    light += (norm) * 0.00001;
+    light += (v_norm) * 0.00001;
 
     vec3 rgb = texture(tex1, v_UV / depth).rgb * light;
     f_color = rgb;
