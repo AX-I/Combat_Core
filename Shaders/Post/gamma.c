15209bd3bbf054826a32a9f5f1d5454d29aa8af3
AX-I <36831950+AX-I@users.noreply.github.com>
2026-04-30 21:15:45 -0400

    CL tonemap ACES

--- Shaders/Post/gamma.c
+++ Shaders/Post/gamma.c
@@ -1,4 +1,26 @@
 
+float3 rtt_and_odt_fit(float3 v) {
+  float3 a = v * (v + 0.0245786f) - 0.000090537f;
+  float3 b = v * (0.983729f * v + 0.4329510f) + 0.238081f;
+  return a / b;
+}
+
+float3 acesMat(float3 px) {
+  float3 j = (float3)(
+    dot(px, (float3)(0.59719f, 0.35458f, 0.04823f)),
+    dot(px, (float3)(0.07600f, 0.90834f, 0.01566f)),
+    dot(px, (float3)(0.02840f, 0.13383f, 0.83777f))
+  );
+  j = rtt_and_odt_fit(j+0.001f);
+  j = (float3)(
+    dot(j, (float3)( 1.60475f, -0.53108f, -0.07367f)),
+    dot(j, (float3)(-0.10208f,  1.10813f, -0.00605f)),
+    dot(j, (float3)(-0.00327f, -0.07276f,  1.07602f))
+  );
+  j = sqrt(j);
+  return j;
+}
+
 __kernel void g(__global ushort *Ro, __global ushort *Go, __global ushort *Bo,
                 const float exposure,
                 const int wF, const int hF, const int BS,
@@ -17,12 +39,17 @@ __kernel void g(__global ushort *Ro, __global ushort *Go, __global ushort *Bo,
 
     for (int cy = h1; cy < min(h2, hF); cy++) {
         for (int cx = ci; cx < wF; cx += stepW) {
-            ushort3 i = (ushort3)(Ro[wF * cy + cx], Go[wF * cy + cx], Bo[wF * cy + cx]);
-            float3 j = sqrt(8*exposure*convert_float3(i)) / 1.1f;
+            float3 j = 8 * exposure * (float3)(Ro[wF * cy + cx], Go[wF * cy + cx], Bo[wF * cy + cx]);
+
+           /*
+            j = sqrt(j) / 1.1f;
             float m = max(j.x, max(j.y, j.z)) / 255.f;
             if (m > 1.f) {
               j = j / sqrt(m);
             }
+           */
+            j = max((float3)0.f, acesMat(j / 65535.f) * 255.f);
+
             Ro[wF * cy + cx] = (ushort)min(255.f, j.x);
             Go[wF * cy + cx] = (ushort)min(255.f, j.y);
             Bo[wF * cy + cx] = (ushort)min(255.f, j.z);
