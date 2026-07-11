212e09a439e50c1281c7e33e8b1c7660d82ff1e0
AX-I <36831950+AX-I@users.noreply.github.com>
2026-07-11 15:17:57 -0400

    {3/4} AVSL update

--- Shaders_src/Templates.py
+++ Shaders_src/Templates.py
@@ -19,7 +19,7 @@
 # ======== ========
 
 # AXI Visualizer Shader Templates
-# v0.2
+# v0.3
 
 template_func = """
 #define TILE_SIZE 16.f
@@ -27,7 +27,7 @@ template_func = """
 #define TILE_AREA 256
 
 __kernel void draw(__global int *TO, __global int *TN,
-                   __global ushort *Ro, __global ushort *Go, __global ushort *Bo,
+                   __global ushort3 *Ro,
                    __global float *F, __global float2 *P, __global float *Z,
                    [SHADER_ARGS]
                    const int wF, const int hF) {
@@ -221,7 +221,7 @@ template_end = """
 template_func_small = """
 
 __kernel void drawSmall(__global int *TO,
-                        __global ushort *Ro, __global ushort *Go, __global ushort *Bo,
+                        __global ushort3 *Ro,
                         __global float *F, __global float2 *P, __global float *Z,
                         [SHADER_ARGS]
                         const int wF, const int hF, const int lenP) {
